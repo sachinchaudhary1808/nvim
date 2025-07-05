f3314b33f0e6dd942fe5c618897fe403d5e1d06b
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-07-05 19:25:22 +0530

    Jose bleh

---
+++ nvim/lua/init.lua
@@ -0,0 +1,35 @@
+-- options
+-- global option keep this at top
+require("options")
+
+-- require("coco.plugins")
+require("plugins.alpha")
+require("plugins.flash-nvim")
+require("plugins.todo-comments")
+require("plugins.mini-pairs")
+require("plugins.fugitive")
+require("plugins.none-ls")
+require("plugins.treesitter")
+require("plugins.auto-session")
+require("plugins.gitsings")
+require("plugins.nvim-tree")
+require("plugins.undotree")
+require("plugins.bufferline-nvim")
+require("plugins.harpoon2")
+require("plugins.sniprun")
+require("plugins.which-key")
+require("plugins.blink-cmp")
+require("plugins.lsp")
+require("plugins.telescope")
+require("plugins.debugging")
+require("plugins.vim-test")
+require("plugins.trouble")
+
+require("utils.compiler")
+require("snippets.snip")
+
+-- files
+require("keymaps")
+require("auto-commands")
+
+require("small")
