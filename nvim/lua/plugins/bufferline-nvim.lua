7289d42ca733d7a548a3af8e6bc91f5172183421
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-07-26 00:43:09 +0530

    bufferline fix

--- nvim/lua/plugins/bufferline-nvim.lua
+++ nvim/lua/plugins/bufferline-nvim.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap
 require("bufferline").setup({
-	["options"] = { ["always_show_bufferline"] = false, ["hover"] = { ["enabled"] = false } },
+    ["options"] = { ["always_show_bufferline"] = false, ["hover"] = { ["enabled"] = false } },
 })
 
 -- bufferline keymaps
@@ -13,6 +13,6 @@ keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
 keymap.set("n", "<leader>bb", "<cmd>e #<CR>", { desc = "Switch to Other Buffer" })
 keymap.set("n", "<leader>br", "<cmd>BufferLineCloseRight<CR>", { desc = "Delete buffer to the right" })
 keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseLeft<CR>", { desc = "Delete buffers to the left" })
-keymap.set("n", "<leader>bo", "<cmd>BufferlineCloseOthers<CR>", { desc = "Delete other buffers" })
+keymap.set("n", "<leader>bo", "<cmd>BufferLineCloseOthers<CR>", { desc = "Delete other buffers" })
 keymap.set("n", "<leader>bp", "<cmd>BufferLineTogglePin<CR>", { desc = "Toggle pin" })
 keymap.set("n", "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<CR>", { desc = "Delete non-pinned buffers" })
