291081fd9054e6168690492dd681d46e234254b9
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-08-04 09:33:14 +0530

    updated

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         default = mnw.lib.wrap pkgs {
           neovim = pkgs.neovim-unwrapped;
           initLua = ''
-            require("config")
+             require("config")
             require('lz.n').load('lazy')
           '';
           extraBinPath = with pkgs; [
@@ -68,84 +68,85 @@
           ];
 
           plugins = {
-            start =
-              [ optimizedTreesitter ]
-              ++ (with pkgs.vimPlugins; [
-                {
-                  # "pname" and "version"
-                  # or "name" is required
+            start = [
+              optimizedTreesitter
+            ]
+            ++ (with pkgs.vimPlugins; [
+              {
+                # "pname" and "version"
+                # or "name" is required
 
-                  name = "direnv-nvim";
+                name = "direnv-nvim";
 
-                  src = pkgs.fetchFromGitHub {
-                    owner = "NotaShelf";
-                    repo = "direnv.nvim";
-                    rev = "4dfc8758a1deab45e37b7f3661e0fd3759d85788";
-                    hash = "sha256-KqO8uDbVy4sVVZ6mHikuO+SWCzWr97ZuFRC8npOPJIE=";
-                  };
+                src = pkgs.fetchFromGitHub {
+                  owner = "NotaShelf";
+                  repo = "direnv.nvim";
+                  rev = "4dfc8758a1deab45e37b7f3661e0fd3759d85788";
+                  hash = "sha256-KqO8uDbVy4sVVZ6mHikuO+SWCzWr97ZuFRC8npOPJIE=";
+                };
 
-                  # Plugins can have other plugins as dependencies
-                  # this is mainly used in nixpkgs
-                  # avoid it if possible
-                  dependencies = [ ];
-                }
-                lz-n
-                lazydev-nvim
-                lspkind-nvim
-                blink-cmp
-                nvim-ts-autotag
-                nvim-colorizer-lua
-                trouble-nvim
-                fidget-nvim
-                alpha-nvim
-                oil-nvim
-                neoformat
-                none-ls-nvim
-                auto-session
-                bufferline-nvim
-                dressing-nvim
-                indent-blankline-nvim
-                neocord
-                nvim-treesitter-context
-                lualine-nvim
-                mini-pairs
-                which-key-nvim
-                markdown-preview-nvim
-                nvim-web-devicons
-                nvim-surround
-                nvim-lspconfig
-                friendly-snippets
-                lspsaga-nvim
-                flash-nvim
-                gitsigns-nvim
-                onedark-nvim
-                dracula-nvim
-                # onedark-nvim
-                plenary-nvim
-                tokyonight-nvim
-                catppuccin-nvim
-                # neodev-nvim
-                luasnip
-                telescope-nvim
-                telescope-file-browser-nvim
-                todo-comments-nvim
-                # nvim-tree-lua
-                telescope-fzf-native-nvim
-                vim-tmux-navigator
-                undotree
-                sniprun
-                vim-fugitive
-                harpoon2
-                rainbow-delimiters-nvim
+                # Plugins can have other plugins as dependencies
+                # this is mainly used in nixpkgs
+                # avoid it if possible
+                dependencies = [ ];
+              }
+              lz-n
+              lazydev-nvim
+              lspkind-nvim
+              blink-cmp
+              nvim-ts-autotag
+              nvim-colorizer-lua
+              trouble-nvim
+              fidget-nvim
+              alpha-nvim
+              oil-nvim
+              neoformat
+              none-ls-nvim
+              auto-session
+              bufferline-nvim
+              dressing-nvim
+              indent-blankline-nvim
+              neocord
+              nvim-treesitter-context
+              lualine-nvim
+              mini-pairs
+              which-key-nvim
+              markdown-preview-nvim
+              nvim-web-devicons
+              nvim-surround
+              nvim-lspconfig
+              friendly-snippets
+              lspsaga-nvim
+              flash-nvim
+              gitsigns-nvim
+              onedark-nvim
+              dracula-nvim
+              # onedark-nvim
+              plenary-nvim
+              tokyonight-nvim
+              catppuccin-nvim
+              # neodev-nvim
+              luasnip
+              telescope-nvim
+              telescope-file-browser-nvim
+              todo-comments-nvim
+              # nvim-tree-lua
+              telescope-fzf-native-nvim
+              vim-tmux-navigator
+              undotree
+              sniprun
+              vim-fugitive
+              harpoon2
+              rainbow-delimiters-nvim
 
-                # debugging
-                nvim-dap
-                nvim-dap-python
-                nvim-dap-ui
+              # debugging
+              nvim-dap
+              nvim-dap-python
+              nvim-dap-ui
 
-                # testing
-                vim-test
-              ]);
+              # testing
+              vim-test
+            ]);
 
             opt = [
             ];
