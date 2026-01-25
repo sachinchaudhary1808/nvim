d2086e0bfc19b9f2180b7d348a03052d069ccc13
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2026-01-25 17:46:01 +0530

    updated

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -36,9 +36,9 @@ vim.loader.enable()
 
 -- Enable spell checking only for markdown files
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown",
-    callback = function()
-        vim.opt_local.spell = true
-        vim.opt_local.spelllang = "en_us"
-    end,
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = "en_us"
+	end,
 })
