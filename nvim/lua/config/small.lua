dbf3e9472c104013cf02f85ac1662fc10fbe29d3
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2026-05-11 19:55:40 +0530

    updated theme choice neovim

--- nvim/lua/config/small.lua
+++ nvim/lua/config/small.lua
@@ -24,7 +24,7 @@ require("colorizer").setup({})
 require("catppuccin").setup({
 	background = {
 		light = "latte",
-		dark = "mocha",
+		dark = "macchiato",
 	},
 })
 vim.cmd.colorscheme("catppuccin")
