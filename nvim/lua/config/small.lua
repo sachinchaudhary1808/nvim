8af6c777c76b2b4289ef4dfffd3dd577d2779418
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2026-05-13 08:48:23 +0530

    updated the colorscheme again

--- nvim/lua/config/small.lua
+++ nvim/lua/config/small.lua
@@ -24,7 +24,7 @@ require("colorizer").setup({})
 require("catppuccin").setup({
 	background = {
 		light = "latte",
-		dark = "macchiato",
+		dark = "mocha",
 	},
 })
 vim.cmd.colorscheme("catppuccin")
